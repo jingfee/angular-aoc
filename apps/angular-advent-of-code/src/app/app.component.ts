14c6e293ff9e00523825d21f63cc36b2d5d0690c
Sebastian Hew <jingfee@gmail.com>
2021-10-24 21:35:29 +0200

    initial commit

---
+++ apps/angular-advent-of-code/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jingfee-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-advent-of-code';
+}
