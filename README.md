9ffe77b0e66b2b783fff4306378f1f84d3c158bf
Sebastian Hew <jingfee@gmail.com>
2023-12-04 08:22:50 +0100

    2023 day 4

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 | 1 | :white_check_mark: | :white_check_mark: |
 | 2 | :white_check_mark: | :white_check_mark: |
 | 3 | :white_check_mark: | :white_check_mark: |
-| 4 | :clock3: | :clock3: |
+| 4 | :white_check_mark: | :white_check_mark: |
 | 5 | :clock3: | :clock3: |
 | 6 | :clock3: | :clock3: |
 | 7 | :clock3: | :clock3: |
--- apps/aoc/src/app/services/solvers/2023/day4.solver.ts
+++ apps/aoc/src/app/services/solvers/2023/day4.solver.ts
@@ -11,33 +11,121 @@ import { IDaySolver } from './solver.service';
 export class Day4Solver implements IDaySolver {
   constructor(
     private aocClient: AocClientService,
-    private utilService: UtilService
+    private utilService: UtilService,
   ) {}
 
   solve_part_one(): Observable<Status> {
     return this.aocClient.getInput(2023, 4).pipe(
       switchMap((input) => {
-        return this.aocClient.postAnswer(2023, 4, 1, undefined);
-      })
+        const parsedInput = this.utilService.rowInputToStringArray(input);
+        const result = this.scratchCards(parsedInput);
+        return this.aocClient.postAnswer(2023, 4, 1, result);
+      }),
     );
   }
 
   solve_part_two(): Observable<Status> {
     return this.aocClient.getInput(2023, 4).pipe(
       switchMap((input) => {
-        return this.aocClient.postAnswer(2023, 4, 2, undefined);
-      })
+        const parsedInput = this.utilService.rowInputToStringArray(input);
+        const result = this.realScratchCards(parsedInput);
+        return this.aocClient.postAnswer(2023, 4, 2, result);
+      }),
     );
   }
 
   test_part_one(): Observable<Status> {
-    const test = false;
+    const test =
+      this.scratchCards([
+        'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
+        'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
+        'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
+        'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
+        'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
+        'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11',
+      ]) === 13;
     return test ? of(Status.SOLVED) : of(Status.ERROR);
   }
 
   test_part_two(): Observable<Status> {
-    const test = false;
+    const test =
+      this.realScratchCards([
+        'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
+        'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
+        'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
+        'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
+        'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
+        'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11',
+      ]) === 30;
 
     return test ? of(Status.SOLVED) : of(Status.ERROR);
+  }
+
+  private scratchCards(cards: string[]) {
+    let sum = 0;
+    for (const card of cards) {
+      const cardSplit = card.split(': ');
+      const numberSplit = cardSplit[1].split(' | ');
+      const winningNumbers = numberSplit[0]
+        .split(' ')
+        .filter((n) => n !== '')
+        .map((n) => parseInt(n));
+      const numbers = numberSplit[1]
+        .split(' ')
+        .filter((n) => n !== '')
+        .map((n) => parseInt(n));
+      let cardPoints = 0;
+      for (const n of numbers) {
+        if (winningNumbers.includes(n)) {
+          cardPoints = cardPoints === 0 ? 1 : cardPoints * 2;
+        }
+      }
+      sum += cardPoints;
+    }
+    return sum;
+  }
+
+  private realScratchCards(cards: string[]) {
+    const sum = new Map<number, number>();
+    const originalCards = this.parseOriginalCards(cards);
+    for (let i = 0; i < originalCards.length; i++) {
+      const originalCard = originalCards[i];
+      let numberOfCards = sum.get(i) ?? 0;
+      numberOfCards++;
+      sum.set(i, numberOfCards);
+
+      let winnings = 0;
+      for (const number of originalCard.numbers) {
+        if (originalCard.winningNumbers.includes(number)) {
+          winnings++;
+        }
+      }
+
+      for (let j = 0; j < winnings; j++) {
+        const cardIndex = i + j + 1;
+        let sumCard = sum.get(cardIndex) ?? 0;
+        sumCard += numberOfCards;
+        sum.set(cardIndex, sumCard);
+      }
+    }
+    return [...sum.values()].reduce((partialSum, a) => partialSum + a, 0);
+  }
+
+  private parseOriginalCards(cards: string[]) {
+    const parsedCards = [];
+    for (const card of cards) {
+      const cardSplit = card.split(': ');
+      const numberSplit = cardSplit[1].split(' | ');
+      const winningNumbers = numberSplit[0]
+        .split(' ')
+        .filter((n) => n !== '')
+        .map((n) => parseInt(n));
+      const numbers = numberSplit[1]
+        .split(' ')
+        .filter((n) => n !== '')
+        .map((n) => parseInt(n));
+      parsedCards.push({ winningNumbers, numbers });
+    }
+    return parsedCards;
   }
 }
