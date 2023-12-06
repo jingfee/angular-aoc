37e56f3be54387c37d40fbedf2ae496b9e7af6f1
Sebastian Hew <jingfee@gmail.com>
2023-12-06 10:23:34 +0100

    2023 day 6

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 | 3 | :white_check_mark: | :white_check_mark: |
 | 4 | :white_check_mark: | :white_check_mark: |
 | 5 | :white_check_mark: | :white_check_mark: |
-| 6 | :clock3: | :clock3: |
+| 6 | :white_check_mark: | :white_check_mark: |
 | 7 | :clock3: | :clock3: |
 | 8 | :clock3: | :clock3: |
 | 9 | :clock3: | :clock3: |
--- apps/aoc/src/app/services/solvers/2023/day6.solver.ts
+++ apps/aoc/src/app/services/solvers/2023/day6.solver.ts
@@ -11,33 +11,107 @@ import { IDaySolver } from './solver.service';
 export class Day6Solver implements IDaySolver {
   constructor(
     private aocClient: AocClientService,
-    private utilService: UtilService
+    private utilService: UtilService,
   ) {}
 
   solve_part_one(): Observable<Status> {
     return this.aocClient.getInput(2023, 6).pipe(
       switchMap((input) => {
-        return this.aocClient.postAnswer(2023, 6, 1, undefined);
-      })
+        const parsedInput = this.utilService.rowInputToStringArray(input);
+        const result = this.race(this.parseInput(parsedInput));
+        return this.aocClient.postAnswer(2023, 6, 1, result);
+      }),
     );
   }
 
   solve_part_two(): Observable<Status> {
     return this.aocClient.getInput(2023, 6).pipe(
       switchMap((input) => {
-        return this.aocClient.postAnswer(2023, 6, 2, undefined);
-      })
+        const parsedInput = this.utilService.rowInputToStringArray(input);
+        const result = this.race(this.parseInput2(parsedInput));
+        return this.aocClient.postAnswer(2023, 6, 2, result);
+      }),
     );
   }
 
   test_part_one(): Observable<Status> {
-    const test = false;
+    const test =
+      this.race(
+        this.parseInput(['Time:      7  15   30', 'Distance:  9  40  200']),
+      ) === 288;
     return test ? of(Status.SOLVED) : of(Status.ERROR);
   }
 
   test_part_two(): Observable<Status> {
-    const test = false;
+    const test =
+      this.race(
+        this.parseInput2(['Time:      7  15   30', 'Distance:  9  40  200']),
+      ) === 71503;
 
     return test ? of(Status.SOLVED) : of(Status.ERROR);
+  }
+
+  private parseInput(input: string[]) {
+    const races = [];
+    const timeSplit = input[0]
+      .split('Time:')[1]
+      .split(' ')
+      .filter((t) => t !== '');
+    const distanceSplit = input[1]
+      .split('Distance:')[1]
+      .split(' ')
+      .filter((t) => t !== '');
+
+    for (let i = 0; i < timeSplit.length; i++) {
+      races.push({
+        time: parseInt(timeSplit[i]),
+        distance: parseInt(distanceSplit[i]),
+      });
+    }
+    return races;
+  }
+
+  private parseInput2(input: string[]) {
+    const timeSplit = input[0]
+      .split('Time:')[1]
+      .split(' ')
+      .filter((t) => t !== '');
+    const distanceSplit = input[1]
+      .split('Distance:')[1]
+      .split(' ')
+      .filter((t) => t !== '');
+
+    let timeString = '';
+    let distanceString = '';
+    for (let i = 0; i < timeSplit.length; i++) {
+      timeString += timeSplit[i];
+      distanceString += distanceSplit[i];
+    }
+    return [
+      {
+        time: parseInt(timeString),
+        distance: parseInt(distanceString),
+      },
+    ];
+  }
+
+  private race(races: { time: number; distance: number }[]) {
+    let prod = 1;
+    for (const race of races) {
+      const c1 = Math.floor(
+        (race.time +
+          Math.sqrt(Math.pow(-1 * race.time, 2) - 4 * (race.distance + 1))) /
+          2,
+      );
+      const c2 = Math.ceil(
+        (race.time -
+          Math.sqrt(Math.pow(-1 * race.time, 2) - 4 * (race.distance + 1))) /
+          2,
+      );
+
+      const waysToWin = c1 - c2 + 1;
+      prod *= waysToWin;
+    }
+    return prod;
   }
 }
